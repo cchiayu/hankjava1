6d235acb0b5c100970045b3ea3bc0ce8253f87ca
cchiayu <love.yy0114@gmail.com>
2018-11-27 11:58:13 +0800

    11/27

---
+++ src/com/tom/PokerGame.java
@@ -0,0 +1,52 @@
+package com.tom;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class PokerGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int flowers[] = {0X2663,0X2666,0X2665,0X2660};
+		System.out.println(flowers);
+		int []nums = new int [5];
+		nums [0]= 98;
+		nums [4]= 78;
+		System.out.println(nums[0]);
+		System.out.println(nums.length);
+		for (int i = 0;i<nums.length;i++){
+			System.out.print(nums[i]);
+		}
+//		Scanner scanner = new Scanner(System.in);
+//		Random random = new Random();
+//		int secret = random.nextInt(13)+1;
+//		int flower = random.nextInt(4);
+//		
+//		/*char[]A = {'S','H','D','C'};
+//		Random k = new Random();
+//		int kind = k.nextInt(4+1);
+//		System.out.println(A [kind]);*/
+//		char c = 0;
+//		System.out.print(secret);
+//		switch(flower){
+//		
+//			case 0: 
+//				c ='S';
+//				break;
+//			case 1:
+//				c='H';
+//				break;
+//			case 2 : 
+//				c = 'D';
+//				break;
+//			case 3:
+//				c = 'C';
+//				break;
+//				
+//			}
+	}
+}
+
+	
+
+
